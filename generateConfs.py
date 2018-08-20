b6a1b524d49ddaae973c119c3f4a52f8bd9c7a85
Nuno Facha <nunofacha@gmail.com>
2018-08-20 12:05:28 +0000

    WIP

--- generateConfs.py
+++ generateConfs.py
@@ -1,5 +1,6 @@
 import os
-folders = ['downloads/jar/stable/craftbukkit', 'downloads/jar/stable/spigot', 'downloads/jar/stable/vanilla']
+folders = ['downloads/jar/stable/craftbukkit', 'downloads/jar/stable/spigot', 'downloads/jar/stable/vanilla',
+           'downloads/jar/snapshot/craftbukkit', 'downloads/jar/snapshot/spigot', 'downloads/jar/snapshot/vanilla']
 for path in folders:
 
     for filename in os.listdir(path):
@@ -7,16 +8,25 @@ for path in folders:
         type = data[0]
         version = data[1].replace(".jar", "")
         template = ""
+        stability = ""
+        filename = ""
+        if "stable" in path:
+            stability = "stable"
+            filename = '%s/%s-%s.jar.conf' % (path.replace("jar", "conf"), type, version)
+        else:
+            stability = "snapshot"
+            filename = '%s/%s-%s-snapshot.jar.conf' % (path.replace("jar", "conf"), type, version)
         if type == "craftbukkit":
-            with open('templates/craftbukkit.template', 'r') as file:
+            with open('templates/%s/craftbukkit.template' % stability, 'r') as file:
                 template = file.read()
         if type == "spigot":
-            with open('templates/spigot.template', 'r') as file:
+            with open('templates/%s/spigot.template' % stability, 'r') as file:
                 template = file.read()
         if type == "vanilla":
-            with open('templates/vanilla.template', 'r') as file:
+            with open('templates/%s/vanilla.template' % stability, 'r') as file:
                 template = file.read()
         template = template.replace("[VERSION]", version)
-        with open('%s/%s-%s.jar.conf' % (path.replace("jar", "conf"), type, version), 'a') as the_file:
+
+        with open(filename, 'a') as the_file:
             the_file.write(template)
         print("Saved conf file for %s-%s.jar.conf" % (type, version))
