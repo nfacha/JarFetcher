a659b1d5522ddf106508ed0010600ea2da0543d7
Unknown <nunofacha@gmail.com>
2018-08-28 17:50:34 +0000

    Typo fix

--- buildDist.py
+++ buildDist.py
@@ -2,7 +2,7 @@ import os
 
 os.system("rm -rf downloads/dist/*.jar")
 os.system("rm -rf downloads/dist/*.jar.conf")
-os.system("rm -rf downloads/dist/*.zipi")
+os.system("rm -rf downloads/dist/*.zip")
 os.system("/usr/bin/find downloads/jar -iname '*.jar' -exec cp --target-directory downloads/dist {} \;")
 os.system("/usr/bin/find downloads/conf -iname '*.jar.conf' -exec cp --target-directory downloads/dist {} \;")
 os.system("cd downloads/dist && /usr/bin/zip -R -D dist.zip *")
