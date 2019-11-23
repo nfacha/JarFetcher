d0e13d522123f36de922c3304d1f005c1b758795
Nuno Facha <nunofacha@gmail.com>
2019-11-23 21:07:23 -0100

    Add SpigotProvider

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 import Storage
-from link_providers import CraftBukkitProvider
+from link_providers import CraftBukkitProvider, SpigotProvider
 
 all_links = {
     'craftbukkit': {},
@@ -12,5 +12,6 @@ Storage.init_logger()
 Storage.logger.info('JarFetcher starting')
 Storage.logger.info('Getting CraftBukkit links')
 all_links['craftbukkit'].update(CraftBukkitProvider.get())
+all_links['spigot'].update(SpigotProvider.get())
 
 Storage.logger.debug(all_links)
