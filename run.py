610aaf134ab934b0089751154f96603e1659e7c0
Nuno Facha <nunofacha@gmail.com>
2019-11-23 22:27:43 -0100

    Generate .sql file

--- run.py
+++ run.py
@@ -4,6 +4,7 @@ import os
 import Storage
 import conf_generator
 import downloader
+import import_generator
 
 all_links = {
     'craftbukkit': {},
@@ -111,3 +112,21 @@ for stage in os.listdir('jar'):
             Storage.logger.info(f'Generating configs for {jar_name}')
             jar_version = jar_name.split('-')[1].replace('.jar', '')
             conf_generator.generate(jar_type, jar_version, stage)
+
+Storage.logger.info('Generating import')
+import_command = ''
+for stage in os.listdir('jar'):
+    for jar_type in os.listdir(f'jar/{stage}'):
+        Storage.logger.info(f'Generating configs for {stage} -> {jar_type}')
+        for jar_name in os.listdir(f'jar/{stage}/{jar_type}'):
+            if jar_name.startswith('.') or os.path.isdir(f'jar/{stage}/{jar_type}/{jar_name}'):
+                continue
+            Storage.logger.info(f'Generating configs for {jar_name}')
+            jar_version = jar_name.split('-')[1].replace('.jar', '')
+            import_command += import_generator.generate(jar_type, jar_version, stage)
+if os.path.exists('import.sql'):
+    os.remove('import.sql')
+
+with open(f'import.sql', 'a') as output_file:
+    output_file.write(import_command)
+    Storage.logger.info('Saved import.sql file')
