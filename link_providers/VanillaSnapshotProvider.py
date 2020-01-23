3333c3a441cc435cd30e620f4c8881e6820516e3
Nuno Facha <nunofacha@gmail.com>
2020-01-23 09:35:09 -0100

    Fuck

--- link_providers/VanillaSnapshotProvider.py
+++ link_providers/VanillaSnapshotProvider.py
@@ -12,12 +12,14 @@ def get():
     driver.get('https://mcversions.net/')
     from datetime import datetime, timedelta
     date_limit = datetime.utcnow() - timedelta(days=30)
-    for version in driver.find_element_by_xpath('//*[@id="content"]/div/div[2]/div[2]/ul').find_elements_by_tag_name(
-            'li'):
-        version_name = version.find_element_by_class_name("version").text
-        version_name = str(version_name).replace('-', '_')
-        version_date = datetime.strptime(version.find_element_by_class_name("time").text, '%m/%d/%y')
-        try:
+    try:
+        for version in driver.find_element_by_xpath(
+                '//*[@id="content"]/div/div[2]/div[2]/ul').find_elements_by_tag_name(
+                'li'):
+            version_name = version.find_element_by_class_name("version").text
+            version_name = str(version_name).replace('-', '_')
+            version_date = datetime.strptime(version.find_element_by_class_name("time").text, '%m/%d/%y')
+
             version_link = version.find_element_by_class_name("server").get_attribute('href')
             import Storage
             if version_date < date_limit:
@@ -26,6 +28,6 @@ def get():
             target_file_name = "vanila-%s-snapshot.jar" % version_name
             Storage.logger.info(f'Found download link for Vanilla Snapshot {version_name}: {version_link}')
             links[target_file_name] = version_link
-        except:
-            pass
+    except:
+        pass
     return links
